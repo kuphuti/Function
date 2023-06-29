fc814be8514988bab2517ff3a0c8817c092e7d2a
kuphuti <108127166+kuphuti@users.noreply.github.com>
2023-06-29 13:35:03 +0530

    Add files via upload

---
+++ example.py
@@ -0,0 +1,28 @@
+# def addition(a,b):
+#     c=a+b
+#     print("addition=",c)
+# addition(2,3)
+
+
+# def addition(a,b):
+#     c=a+b
+#     print(c)
+# x=int(input("enter x"))
+# y=int(input("enter y")) 
+# addition(x,y)
+
+# def add(a,b):
+#     c=a+b
+#     return c
+# # x=int(input("enter a"))
+# y=int(input("enter b"))
+# sum=add(x,y)
+# print("addition=",sum)
+
+# def message():
+#     print("hello python")
+# message()
+
+def xyz():
+    print("working with recursion")
+xyz()
